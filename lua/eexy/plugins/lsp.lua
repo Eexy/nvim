8e6d653021afb8924dc8384279feb920d7b98f5d
Boussif Isai <eexdhe@gmail.com>
2024-03-24 14:32:22 +0100

    feat: add plugins

--- lua/eexy/plugins/lsp.lua
+++ lua/eexy/plugins/lsp.lua
@@ -24,6 +24,66 @@ return {
 			masonConfig.setup({
 				handlers = {
 					lsp_zero.default_setup,
+					jsonls = function()
+						require("lspconfig").jsonls.setup({
+							filetypes = { "json", "jsonc" },
+							settings = {
+								json = {
+									-- Schemas https://www.schemastore.org
+									schemas = {
+										{
+											fileMatch = { "package.json" },
+											url = "https://json.schemastore.org/package.json",
+										},
+										{
+											fileMatch = { "tsconfig*.json" },
+											url = "https://json.schemastore.org/tsconfig.json",
+										},
+										{
+											fileMatch = {
+												".prettierrc",
+												".prettierrc.json",
+												"prettier.config.json",
+											},
+											url = "https://json.schemastore.org/prettierrc.json",
+										},
+										{
+											fileMatch = { ".eslintrc", ".eslintrc.json" },
+											url = "https://json.schemastore.org/eslintrc.json",
+										},
+										{
+											fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+											url = "https://json.schemastore.org/babelrc.json",
+										},
+										{
+											fileMatch = { "lerna.json" },
+											url = "https://json.schemastore.org/lerna.json",
+										},
+										{
+											fileMatch = { "now.json", "vercel.json" },
+											url = "https://json.schemastore.org/now.json",
+										},
+										{
+											fileMatch = {
+												".stylelintrc",
+												".stylelintrc.json",
+												"stylelint.config.json",
+											},
+											url = "http://json.schemastore.org/stylelintrc.json",
+										},
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+
+			local cmp = require("cmp")
+			cmp.setup({
+				preselect = "item",
+				completion = {
+					completeopt = "menu,menuone,noinsert",
 				},
 			})
 		end,
